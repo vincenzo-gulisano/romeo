e739f5b056962834e63c7ef57b2621e9b8ba7192
vincenzo-gulisano <vincenzo.gulisano@chalmers.se>
2024-02-25 20:39:25 +0100

    updated scripts

--- scripts/create_plots_for_exp5.sh
+++ scripts/create_plots_for_exp5.sh
@@ -3,19 +3,19 @@ base_exp_folder="./data/jingyu/exp5.1"
 exp_folder="${base_exp_folder}/001-100"
 episodes=100
 
-# rm ${exp_folder}/episodesstats.csv 
-# rm ${base_exp_folder}/compressionandepisodesstats.csv
+rm ${exp_folder}/episodesstats.csv 
+rm ${base_exp_folder}/compressionandepisodesstats.csv
 
-# echo "Creating extra stats"
-# grep -Eo '[0-9]+,[0-9]+,action [0-9]+' ${exp_folder}/episodes.csv | sed -E 's/,action /,/' | cut -d, -f1,3 > ${exp_folder}/actions.csv
-# awk '/^Got a new state\/reward pair: / {sub(/^Got a new state\/reward pair: /, ""); print int($1)} /^reward / {sub(/^reward /, ""); print $1}' ${exp_folder}/python_agent.log | paste -d, - -  > ${exp_folder}/rewards.csv
-# awk -F',' 'BEGIN {OFS=","; sum=0} {sum += $2; print $1, sum}' ${exp_folder}/rewards.csv > ${exp_folder}/cumulativereward.csv
-# awk -F',' 'NR > 1 { print $1 "," ($2 < 1000 ? 0 : 1) }' ${exp_folder}/latency.average.csv > ${exp_folder}/latency.violations.csv
+echo "Creating extra stats"
+grep -Eo '[0-9]+,[0-9]+,action [0-9]+' ${exp_folder}/episodes.csv | sed -E 's/,action /,/' | cut -d, -f1,3 > ${exp_folder}/actions.csv
+awk '/^Got a new state\/reward pair: / {sub(/^Got a new state\/reward pair: /, ""); print int($1)} /^reward / {sub(/^reward /, ""); print $1}' ${exp_folder}/python_agent.log | paste -d, - -  > ${exp_folder}/rewards.csv
+awk -F',' 'BEGIN {OFS=","; sum=0} {sum += $2; print $1, sum}' ${exp_folder}/rewards.csv > ${exp_folder}/cumulativereward.csv
+awk -F',' 'NR > 1 { print $1 "," ($2 < 1000 ? 0 : 1) }' ${exp_folder}/latency.average.csv > ${exp_folder}/latency.violations.csv
 
-# echo "Creating plots"
-# python plotting/plot_experiment_stats_exp5.py --print_global_events ${exp_folder}/ ${exp_folder}/episodesstats.csv 
+echo "Creating plots"
+python plotting/plot_experiment_stats_exp5.py --print_global_events ${exp_folder}/ ${exp_folder}/episodesstats.csv 
 
-# python plotting/append_episodesstatscsv_to_global_one.py ${exp_folder}/episodesstats.csv ${base_exp_folder}/compressionandepisodesstats.csv DQNAgent-001-100
+python plotting/append_episodesstatscsv_to_global_one.py ${exp_folder}/episodesstats.csv ${base_exp_folder}/compressionandepisodesstats.csv DQNAgent-001-100
 
 python plotting/create_stat_episodes_boxplot_for_compressions.py ${base_exp_folder}/compressionandepisodesstats.csv ${base_exp_folder}/ rewards
 python plotting/create_stat_episodes_boxplot_for_compressions.py ${base_exp_folder}/compressionandepisodesstats.csv ${base_exp_folder}/ ratio.percent
