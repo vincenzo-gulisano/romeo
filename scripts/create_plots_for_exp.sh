df268801e75870a7d89323146aec530e344c8e0f
Vincenzo Gulisano <vincenzo.gulisano@chalmers.se>
2024-04-29 15:43:38 +0200

    committing plotting scripts updates

--- scripts/create_plots_for_exp.sh
+++ scripts/create_plots_for_exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-base_exp_folder="./data/exp12/linearroad/agent"
-folders_to_explore=(12.1 12.2)
+base_exp_folder="./data/exp13/synthetic/agent"
+folders_to_explore=(13.2)
 for folder_to_explore in "${folders_to_explore[@]}"; do
 
     exp_folder="${base_exp_folder}/${folder_to_explore}"
