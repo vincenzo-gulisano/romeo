b024ca1dc4cb481f8d9e9fe31acd07127e5c07e5
Vincenzo Gulisano <vincenzo.gulisano@chalmers.se>
2024-09-27 10:28:41 +0200

    updated policy barriers names

--- src/main/java/com/vincenzogulisano/javapythoncommunicator/PolicyBarrier.java
+++ src/main/java/com/vincenzogulisano/javapythoncommunicator/PolicyBarrier.java
@@ -1,8 +1,8 @@
 package com.vincenzogulisano.javapythoncommunicator;
 
 public enum PolicyBarrier {
-    WEAOB, // Wallclock, Event time, and Aggregate OBlivious
-    EAOB, // Event time, and Aggregate OBlivious
-    AOB, // Aggregate OBlivious
-    WEAAW;// Wallclock, Event time, and Aggregate Aware
+    WELOB, // Wallclock, Event time, and Latency OBlivious
+    ELOB, // Event time, and Latency OBlivious
+    LOB, // Latency OBlivious
+    WELAW;// Wallclock, Event time, and Latency Aware
 }
