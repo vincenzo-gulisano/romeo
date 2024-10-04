7797cd00fe4754f9869528cc041c29f633cc7da1
Vincenzo Gulisano <vincenzo.gulisano@chalmers.se>
2024-10-04 12:56:31 +0200

    checking results

--- plotting/paper_plot_baselines_vs_agent_icpe_single_column.py
+++ plotting/paper_plot_baselines_vs_agent_icpe_single_column.py
@@ -110,7 +110,7 @@ def plot_graphs(
 
     initial_opacity = 0.3
     final_opacity = 0.9
-    splits = 3  # Number of portions to divide the subset into
+    splits = 4  # Number of portions to divide the subset into
     max_line_width = 2.5  # Set your desired maximum line width
     agent_color = "green"
 
