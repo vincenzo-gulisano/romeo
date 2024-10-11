9dbc7f2014dc1972ffb8e8d56200f288ffa90a0e
Vincenzo Gulisano <vincenzo.gulisano@chalmers.se>
2024-10-11 11:52:56 +0000

    start python agent bootstrap server and base folder

--- scripts/start_python_agent.sh
+++ scripts/start_python_agent.sh
@@ -2,9 +2,10 @@
 episodes=$1
 steps=$2
 log_folder=$3
+bootstrap_server=$4
 
 
-python ./python/DQNAgent.py ${episodes} ${steps} > ${log_folder}/python_agent.log &
+python ./python/DQNAgent.py ${episodes} ${steps} ${bootstrap_server} ${log_folder} > ${log_folder}/python_agent.log &
 
 # Capture the process ID (PID) of the last background command
 pid=$!
