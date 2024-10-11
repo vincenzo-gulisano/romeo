76075e11b704351bfc4927c5d53280f4210b9e9b
Vincenzo Gulisano <vincenzo.gulisano@chalmers.se>
2024-10-11 12:21:00 +0200

    fix order bug in stats computation (q3 and mean could be swapped)

--- plotting/plot_experiment_stats_exp.py
+++ plotting/plot_experiment_stats_exp.py
@@ -100,6 +100,10 @@ def plot_files_in_folder(folder,episodesstatsfile,makeplots,print_global_events,
     episode_folder = os.path.join(folder, 'eps')
     os.makedirs(episode_folder, exist_ok=True)
 
+    # Define the consistent column order
+    column_order = ['episode', 'stat', 'q1', 'q2', 'q3', 'sum', 'mean']
+
+
     # Iterate through unique episode values in episodes_df
     for episode_value in episodes_df['episode'].unique():
 
@@ -178,7 +182,7 @@ def plot_files_in_folder(folder,episodesstatsfile,makeplots,print_global_events,
                 stats_df = pd.DataFrame(stats)
 
                 # Append the DataFrame to the output CSV file
-                stats_df.to_csv(episodesstatsfile, mode='a', index=False, header=not os.path.exists(episodesstatsfile))
+                stats_df.to_csv(episodesstatsfile, mode='a', index=False, header=not os.path.exists(episodesstatsfile), columns=column_order)
 
 
         if makeplots and somethingPlotted:
