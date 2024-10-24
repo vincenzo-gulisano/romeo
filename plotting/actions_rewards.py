d245506aab5e276fbb454a958eb703798fcb1e98
Vincenzo Massimiliano Gulisano <vincenzo.gulisano@chalmers.se>
2024-10-24 06:27:03 +0200

    entropy plotting

--- plotting/actions_rewards.py
+++ plotting/actions_rewards.py
@@ -8,12 +8,30 @@ def find_reward_files(folder):
         for file in files:
             if file == 'rewards.actions.csv':
                 file_path = os.path.join(root, file)
-                process_reward_file(file_path)
+                process_reward_file_rewards_actions(file_path)
+            if file == 'probs.csv':
+                file_path = os.path.join(root, file)
+                process_reward_file_probs_file(file_path)
 
-def process_reward_file(file_path):
+def process_reward_file_rewards_actions(file_path):
     # Read the CSV without headers (as per the problem statement)
     try:
         df = pd.read_csv(file_path, header=None, names=['ts', 'reward'])
+        
+        # Count the occurrences of each unique reward
+        reward_counts = df['reward'].value_counts()
+        
+        # Print the full path and the occurrences of each reward
+        print(f"{file_path}: {reward_counts}")
+        print("\n")
+        
+    except Exception as e:
+        print(f"Error processing file {file_path}: {e}")
+
+def process_reward_file_probs_file(file_path):
+    # Read the CSV without headers (as per the problem statement)
+    try:
+        df = pd.read_csv(file_path, header=None, names=['episode','step','prob0','prob1','prob2','entropy','entropy_ma','entropy_ma_ready','reward'])
         
         # Count the occurrences of each unique reward
         reward_counts = df['reward'].value_counts()
