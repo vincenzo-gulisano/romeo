cacd6348c378314ae3369067f2629172f76b3a47
vincenzo-gulisano <vincenzo.gulisano@chalmers.se>
2025-01-07 07:10:06 +0100

    Made state_measurement_check_period required param

--- python/CCRAgent.py
+++ python/CCRAgent.py
@@ -130,7 +130,7 @@ class SPEEnvironment(Env):
         # Wait for the state and reward measurement
         self.prev_stat_time = time.time()
         state_measurement_available = False
-        print('Waiting for new observation')
+        print(f"{datetime.datetime.now()} - Waiting for new observation", flush=True)
         while not state_measurement_available:
             time.sleep(self.state_measurement_check_period)
             with self.consumer.tracker.data_lock: # This is to ensure this thread does not read previous_values while they are being updated by other threads
@@ -273,10 +273,10 @@ if __name__ == "__main__":
     parser.add_argument('episodes', help='Number of episodes')
     parser.add_argument('steps', help='Number of steps')
     parser.add_argument('compression', help='Compression level')
-    parser.add_argument('--state_measurement_check_period', help='How many seconds to sleep in between checks for sstate measurements (optional, default is 1.0)', default=1.0, type=float)
+    parser.add_argument('state_measurement_check_period', help='How many seconds to sleep in between checks for state measurements')
     args = parser.parse_args()
     
-    env = SPEEnvironment(int(args.steps),args.state_measurement_check_period)
+    env = SPEEnvironment(int(args.steps),float(args.state_measurement_check_period))
 
     for i in range(int(args.episodes)):
         print('starting episode',i+1)
